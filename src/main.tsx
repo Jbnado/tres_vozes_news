ace30cc844d4dea8efb01d54e1d815a642492edd
DouglasSerafim <douglas.serafim@outlook.com.br>
2023-06-16 21:06:19 -0300

    create aabout pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HomePage, SignIn, SignUp, TopicPage } from "./ui";
+import { AboutCreatorsPage, AboutProjectPage, AboutUsPage, HomePage, SignIn, SignUp, TopicPage } from "./ui";
 import "./index.css";
 import { HeaderComponent } from "./ui/components";
 import { AuthProvider, PrivateRoute } from "./context";
@@ -23,6 +23,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
           <HeaderComponent />
           <Routes>
             <Route index element={<HomePage />} />
+            <Route path="about-us" element={<AboutUsPage />} />
+            <Route path="about-project" element={<AboutProjectPage />} />
+            <Route path="about-creators" element={<AboutCreatorsPage />} />
             <Route path="login" element={<SignIn />} />
             <Route path="register" element={<SignUp />} />
             <Route element={<PrivateRoute />}>
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,6 @@ export { default as HomePage } from './home';
 export { default as SignIn } from './sign-in';
 export { default as SignUp } from './sign-up';
 export { default as TopicPage } from './topic';
+export { default as AboutUsPage } from './about-us';
+export { default as AboutProjectPage } from './about-project';
+export { default as AboutCreatorsPage } from './about-creators';
